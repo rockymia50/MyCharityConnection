83eedd74117eec31985708449acddb48d4d0b54c
Roger McIntosh <rogermcintosh@Rogers-Air.home>
2018-02-18 23:20:27 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -35,3 +35,21 @@ $(document).ready(function () {
 
     });
 });
+
+
+// Slideshow auto animations
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 3000); // Change image every 3 seconds
+}
